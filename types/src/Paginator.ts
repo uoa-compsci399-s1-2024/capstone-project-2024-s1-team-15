5f6efd2dcaec01bca3f3ddfdea8a7435e3c099b1
Hunter <kche617@aucklanduni.ac.nz>
2024-04-02 23:46:05 +1100

    add tests for paginator

--- types/src/Paginator.ts
+++ types/src/Paginator.ts
@@ -13,15 +13,15 @@ export default class Paginator<T extends {}> implements IPaginator<T> {
      * The default constructor for the Paginator.
      * @param type The type of data that the Paginator holds.
      */
-    constructor(type: (new(obj: T) => T));
+    constructor(type: (new(obj: object) => T));
 
     /**
      * The overloaded constructor that takes in an object representing a Paginator.
      * @param type The type of data that the Paginator holds.
      * @param obj The object representing a Paginator.
      */
-    constructor(type: (new(obj: T) => T), obj: Partial<IPaginator<T>>);
-    constructor(type: (new(obj: T) => T), obj?: Partial<IPaginator<T>>) {
+    constructor(type: (new(obj: object) => T), obj: Partial<IPaginator<T>>);
+    constructor(type: (new(obj: object) => T), obj?: Partial<IPaginator<T>>) {
         this.data = []
         if (obj?.data) {
             obj.data.forEach((v: T) => {
