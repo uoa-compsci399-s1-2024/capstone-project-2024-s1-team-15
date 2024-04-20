e9911df7af4bc1f62985252607156123c463ad6b
Hunter Chen <kche617@aucklanduni.ac.nz>
2024-04-20 18:44:33 +1200

    update workflows to include env variables

--- backend/src/util/helper.types.ts
+++ backend/src/util/helper.types.ts
@@ -14,7 +14,6 @@ export interface ArrayResult<T> {
     results: T[]
 }
 
-
 export interface InputValidationError<T> {
     field: keyof T
     message: string
--- frontend/app/consts.ts
+++ frontend/app/consts.ts
@@ -1,2 +1,22 @@
-export const API_URI = "http://localhost:3000"
+let uri;
+
+switch (process.env.ENV) {
+    case "LOCAL": {
+        uri = "http://localhost:3000"
+        break
+    }
+    case "DEV": {
+        uri = "https://dev-api.aapc-nz.org"
+        break
+    }
+    case "PROD": {
+        uri = "https://api.aapc-nz.org"
+        break
+    }
+    default: {
+        uri = "http://localhost:3000"
+    }
+}
+
+export const API_URI = uri
 export const WEBSITE_NAME = "Aotearoa Airborne Pollen Collective"
