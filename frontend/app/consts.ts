532f3fc37535c8ba6d4dba130ec3f25bb836dfa7
Arnav <arnard76@gmail.com>
2024-04-24 21:03:54 +1200

    requirements (tests) for login
    - login test uses mock email & password
    
    so don't have to update environment variables everytime password is changed/reset
    
    putting all routes in the constants to be used by tests & development
    
    logged in text alteration

--- frontend/app/consts.ts
+++ frontend/app/consts.ts
@@ -1,22 +1,41 @@
-let apiUri;
+let apiUri, uiUri;
 
 switch (process.env.ENV) {
-    case "LOCAL": {
-        apiUri = "http://localhost:3000"
-        break
-    }
-    case "DEV": {
-        apiUri = "https://dev-api.aapc-nz.org"
-        break
-    }
-    case "PROD": {
-        apiUri = "https://api.aapc-nz.org"
-        break
-    }
-    default: {
-        apiUri = "http://localhost:3000"
-    }
+  case "LOCAL": {
+    apiUri = "http://localhost:3000";
+    uiUri = "http://localhost:3001";
+    break;
+  }
+  case "DEV": {
+    apiUri = "https://dev-api.aapc-nz.org";
+    uiUri = "https://dev.aapc-nz.org";
+    break;
+  }
+  case "PROD": {
+    apiUri = "https://api.aapc-nz.org";
+    uiUri = "https://aapc-nz.org";
+    break;
+  }
+  default: {
+    apiUri = "http://localhost:3000";
+    uiUri = "http://localhost:3001";
+  }
 }
 
-export const API_URI = apiUri  // can add custom uri for testing here
-export const WEBSITE_NAME = "Aotearoa Airborne Pollen Collective"
+export const API_URI = apiUri; // can add custom uri for testing here
+export const WEBSITE_NAME = "Aotearoa Airborne Pollen Collective";
+
+export const UI_URL = uiUri;
+
+export const ROUTES = {
+  LOGIN: "/login",
+  FORGOT_PASSWORD: "/forgot-password",
+
+  REDIRECT_AFTER_LOGIN: "/",
+};
+
+export const URLS = Object.fromEntries(
+  Object.entries(ROUTES).map(([name, route]) => [name, UI_URL + route])
+);
+
+export const OPEN_CMS_ROUTES = [ROUTES.LOGIN, ROUTES.FORGOT_PASSWORD]; // no auth required for these ones
