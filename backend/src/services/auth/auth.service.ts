c9783e21a992b6abd1605c8f10dfd5505757c462
Hunter Chen <kche617@aucklanduni.ac.nz>
2024-04-28 16:45:58 +1200

    fix environment variables on deployment environments; improve environment variable loading

--- backend/src/services/auth/auth.service.ts
+++ backend/src/services/auth/auth.service.ts
@@ -3,34 +3,33 @@ import { Nullable } from "@/util/types/util.types"
 
 export default class AuthContext {
     private readonly authServiceProvider: IAuthService
+    private readonly jwtSecret: string
 
-    constructor(authServiceProvider: IAuthService) {
+    constructor(authServiceProvider: IAuthService, jwtSecret: string = "local") {
         this.authServiceProvider = authServiceProvider
+        this.jwtSecret = jwtSecret
     }
 
     private issueToken(username: string): string {
-        const secret = process.env.JWT_SECRET ?? "local"
         return sign(
             {
                 username: username,
-                roles: ["user"],
+                scopes: ["user", "admin"],
             },
-            secret
+            this.jwtSecret
         )
     }
 
     async login(username: string, password: string): Promise<Nullable<string>> {
-        if (!(await this.authServiceProvider.authenticateUser(username, password))) {
-            return null
+        if (await this.authServiceProvider.authenticateUser(username, password)) {
+            return this.issueToken(username)
         }
-
-        return this.issueToken(username)
+        return null
     }
 
     verifyToken(token: string): Nullable<string | JwtPayload> {
-        const secret = process.env.JWT_SECRET ?? "local"
         try {
-            return verify(token, secret)
+            return verify(token, this.jwtSecret)
         } catch (err) {
             return null
         }
