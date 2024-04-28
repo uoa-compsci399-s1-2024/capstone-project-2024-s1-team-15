31719fb1e279eeca082006b36897c12a34a078df
Arnav <arnard76@gmail.com>
2024-04-29 08:25:22 +1200

    store `pollenValues` as list rather than object

--- frontend/app/(cms)/pollen/edit/PollenDataType.ts
+++ frontend/app/(cms)/pollen/edit/PollenDataType.ts
@@ -5,5 +5,5 @@ export type PollenData = {
     // key is date and value is the pollen value at that date
     // units for pollen values are: pollen grains per cubic metre of air
     // undefined means there is no pollen data for that date
-    pollenValues: { [date: string]: number | undefined }
+    pollenValues: { date: string; value: number | undefined }[]
 }[]
