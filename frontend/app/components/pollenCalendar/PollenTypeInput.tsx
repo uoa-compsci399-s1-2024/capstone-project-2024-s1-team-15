8f271077f205621109599f45bf56b464f6e439ed
Arnav <arnard76@gmail.com>
2024-04-29 19:29:12 +1200

    pollen type filter for pollen calendar component

---
+++ frontend/app/components/pollenCalendar/PollenTypeInput.tsx
@@ -0,0 +1,19 @@
+import Select from "react-select"
+
+export default function PollenTypeInput({
+    allPollenTypes,
+    showPollenTypes,
+}: {
+    allPollenTypes: string[]
+    showPollenTypes: any
+}) {
+    return (
+        <Select
+            closeMenuOnSelect={false}
+            onChange={(selectedOptions) => showPollenTypes(selectedOptions.map(({ value }) => value))}
+            isMulti
+            options={allPollenTypes.map((name) => ({ value: name, label: name }))}
+            // styles={colourStyles}
+        />
+    )
+}
--- frontend/app/components/pollenCalendar/index.tsx
+++ frontend/app/components/pollenCalendar/index.tsx
@@ -11,30 +11,62 @@ export default function PollenCalendar({ pollenData }: { pollenData: PollenData 
     const dateLowerLimit = dayjs("2024-11-28").valueOf()
     const dateUpperLimit = dayjs("2024-12-2").valueOf()
     const [showingDateFilter, showDateFilter] = useState(false)
+
     const [showingPollenTypeFilter, showPollenTypeFilter] = useState(false)
     const [allPollenTypes, setAllPollenTypes] = useState<string[]>([])
     const [showingPollenTypes, showPollenTypes] = useState<string[]>([])
 
     const [formattedPollenData, setFormattedData] = useState<null | FormattedPollenData>(null)
+    const [filteredPollenData, setFilteredPollenData] = useState<null | FormattedPollenData>(null)
 
     useEffect(() => {
         const formatted = formatPollenData(pollenData)
         setFormattedData(formatted)
+
         const allPollenNames = formatted.pollenTypes.map((pollenType) => pollenType)
         setAllPollenTypes(allPollenNames)
         showPollenTypes(allPollenNames)
-        console.log({ allPollenNames })
     }, [pollenData])
+
+    useEffect(() => {
+        if (!formattedPollenData || !showingPollenTypes.length) return setFilteredPollenData(null)
+
+        const filtered: FormattedPollenData = {
+            dailyTotals: formattedPollenData.dailyTotals,
+            pollenTypes: [],
+            pollenValues: [],
+        }
+
+        formattedPollenData.pollenTypes.map((pollenType, index) => {
+            if (!showingPollenTypes.includes(pollenType)) return
+
+            filtered.pollenTypes.push(pollenType)
+            filtered.pollenValues.push(formattedPollenData.pollenValues[index])
+        })
+
+        setFilteredPollenData(filtered)
+    }, [formattedPollenData, showingPollenTypes])
 
     return (
         <>
             <div className="flex flex-col max-w-[100vw] h-[80vh]">
-                {formattedPollenData && showingPollenTypes.length ? (
+                <div className="flex items-center gap-2">
+                    <p>Filter by</p>
+                    <button onClick={() => showPollenTypeFilter((currentState) => !currentState)}>Pollen Type</button>
+                    {allPollenTypes && showingPollenTypeFilter && (
+                        <PollenTypeInput allPollenTypes={allPollenTypes} showPollenTypes={showPollenTypes} />
+                    )}
+
+                    <button onClick={() => showDateFilter((currentState) => !currentState)}>Date</button>
+                    {showingDateFilter && <DateInput lowerLimit={dateLowerLimit} upperLimit={dateUpperLimit} />}
+                </div>
+
+                {filteredPollenData && showingPollenTypes.length ? (
                     <div className="flex flex-col w-full h-[100vh]">
                         <MultiChart
                             dateUpperLimit={dateUpperLimit}
                             dateLowerLimit={dateLowerLimit}
-                            pollenData={formattedPollenData}></MultiChart>
+                            pollenData={filteredPollenData}></MultiChart>
                     </div>
                 ) : (
                     <p>No pollen types selected 🥲</p>
