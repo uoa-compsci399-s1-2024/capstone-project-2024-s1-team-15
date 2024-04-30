6622438ab2b203b53fb1ae30faa4c28d85a4b1c6
Hunter Chen <kche617@aucklanduni.ac.nz>
2024-05-01 06:03:37 +1200

    refactor code

--- frontend/app/(cms)/pollen/edit/page.tsx
+++ frontend/app/(cms)/pollen/edit/page.tsx
@@ -1,98 +1,97 @@
 "use client"
 
 import React, { useEffect, useState, useRef } from "react"
-import { parse } from "./parseExcel"
+import { parseSpreadsheet } from "./parseExcel"
 
-import assumptions from "./assumptions made to parse excel file"
+import assumptions from "./assumptions"
 import { PollenData } from "./PollenDataType"
 
+type ParseError = {
+    message: string
+    errors?: string[]
+}
+
+type Nullable<T> = T | null
+
 export default function EditPollen() {
-    const [errorMessages, setErrorMessage] = useState(null as string[] | null)
     const fileInputReference = useRef(null)
-    const [inputFileWithValidFileType, setInputFileWithValidFileType] = useState(null as null | File)
-    const [pollenDataset, setPollenDataset] = useState(null as null | PollenData)
+
+    const [error, setError] = useState<Nullable<ParseError>>(null)
+    const [inputFile, setInputFile] = useState<Nullable<File>>(null)
+    const [pollenDataset, setPollenDataset] = useState<Nullable<PollenData[]>>(null)
 
     useEffect(() => {
-        if (errorMessages) setInputFileWithValidFileType(null)
-    }, [errorMessages])
+        if (error) setInputFile(null)
+    }, [error])
 
     useEffect(() => {
-        if (!inputFileWithValidFileType) return
-
-        inputFileWithValidFileType.arrayBuffer().then((res) => {
-            const parseResults = parse(res)
-
-            setPollenDataset(parseResults.pollenDataset)
-
-            if (parseResults.errors.length) {
-                setErrorMessage([
-                    "These errors occured while trying to parse (understand) the excel spreadsheet:",
-                    ...parseResults.errors,
-                ])
+        if (!inputFile) return
+        inputFile.arrayBuffer().then((res) => {
+            const parseResults = parseSpreadsheet(res)
+            if (parseResults.errors) {
+                setError({
+                    message: "These errors occurred while trying to parse the Excel spreadsheet:",
+                    errors: parseResults.errors
+                })
+            } else {
+                setPollenDataset(parseResults.pollenDataset)
             }
         })
-    }, [inputFileWithValidFileType])
+    }, [inputFile])
 
     function validateInputFileType(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
         e.preventDefault()
 
         if (!fileInputReference.current) return
-
         const fileInputElement: HTMLInputElement = fileInputReference.current
-
-        if (!fileInputElement.files?.length || !fileInputElement.files[0]) return setErrorMessage(["No file uploaded"])
-
+        if (!fileInputElement.files?.length || !fileInputElement.files[0]) {
+            return setError({
+                message: "No file uploaded."
+            })
+        }
         const uploadedFile = fileInputElement.files[0]
-
         console.log({ uploadedFile })
-
-        if (!uploadedFile.name.includes(".xlsx"))
-            return setErrorMessage([
-                `The file you have uploaded doesn't seem to be an Excel spreadsheet. The filename '${uploadedFile.name}' doesn't have '.xlsx'`,
-            ])
-
-        setErrorMessage(null)
-        setInputFileWithValidFileType(uploadedFile)
+        if (!uploadedFile.name.endsWith(".xlsx")) {
+            return setError({
+                message: "Uploaded file is not a .xlsx Excel spreadsheet."
+            })
+        }
+        setError(null)
+        setInputFile(uploadedFile)
     }
 
     return (
         <>
             <form className="flex flex-col items-start gap-2">
                 <label>
-                    <span>Upload .xlsx spreadsheet file with pollen data</span>
-                    <br />
+                    <p>Upload .xlsx Excel spreadsheet containing pollen data</p>
                     <input type="file" ref={fileInputReference} />
                 </label>
-
-                <button type="submit" className="button" onClick={validateInputFileType}>
-                    Preview data
-                </button>
-                {errorMessages?.length &&
-                    (errorMessages.length === 1 ? (
-                        <p className="form-error">{errorMessages[0]}</p>
-                    ) : (
-                        <>
-                            <p className="form-error font-bold"> {errorMessages[0]} </p>
-                            <ul className="list-disc pl-4 mt-5">
-                                {errorMessages.slice(1).map((msg) => {
-                                    return (
-                                        <li key={msg}>
-                                            <p className="form-error">{msg}</p>
-                                        </li>
-                                    )
-                                })}
-                            </ul>
-                        </>
-                    ))}
+                <button type="submit" className="button" onClick={validateInputFileType}>Preview data</button>
+                {error && (error.errors
+                    ? <>
+                        <p className="form-error font-bold"> {error.message} </p>
+                        <ul className="list-disc pl-4 mt-5">
+                            {error.errors.map((msg) => {
+                                return (
+                                    <li key={msg}>
+                                        <p className="form-error">{msg}</p>
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                    </>
+                    : <p className="form-error">{error.message}</p>
+                )}
             </form>
 
-            {inputFileWithValidFileType && pollenDataset && <div>Preview generated ✅</div>}
+            {inputFile && pollenDataset && <div>Preview generated ✅</div>}
 
             <section className="mt-20">
-                <h3 className="text-4xl mb-5">Assumptions ⚠️</h3>
+                <h2 className="text-2xl font-bold mb-5">Parser Assumptions ️</h2>
                 <p>
-                    The parsing algorithm that attempts to understand your Excel file input makes the following
-                    assumptions:
+                    The parsing algorithm that attempts to understand your Excel spreadsheet
+                    operates under these assumptions:
                 </p>
                 <ul className="list-disc pl-4 mt-5">
                     {assumptions.map((assumption) => {
