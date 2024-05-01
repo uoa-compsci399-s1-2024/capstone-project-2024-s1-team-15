08f42a08dfcf93b63ce1a458ffee968fad518404
Samuel <samuel.k.0227@gmail.com>
2024-05-01 21:54:53 +1200

    top nav edit

--- frontend/app/components/Nav.tsx
+++ frontend/app/components/Nav.tsx
@@ -4,21 +4,21 @@ import Link from "next/link"
 export default function Nav(): React.JSX.Element {
     return (
         <nav>
-            <div className={"h-48 w-full fixed bg-gradient-to-b from-white -z-10"}></div>
-            <div className={`fixed h-28 w-full bg-gradient-to-b from-green-100 flex justify-center items-center z-50`}>
-                <Link href={"/"}>
-                    <div className={"py-6 px-12 flex-col flex justify-center items-center"}>
-                        <span className={"font-bold text-2xl text-black tracking-tight"}>
-                            Aotearoa Airborne Pollen Collective
-                        </span>
-                        <div className={"space-x-2"}>
-                            <span className={"text-secondary text-lg"}>Prototype Website</span>
-                            <span className={"text-secondary text-sm"}>Team Bit by Bit</span>
-                        </div>
-                    </div>
-                </Link>
+            <div className={`fixed h-28 w-full flex justify-between items-center z-50`}>
+                <div className={"pl-20"}>
+                    <span className={"font-bold text-5xl text-black tracking-tight"}>
+                        AAPC
+                    </span>
+                </div>
+                <div className={"pr-20"}>
+                    <Link href={"/login"}>
+                        <button className={"py-2 px-6 rounded-full text-black font-semibold text-xs w-28"} style={{ backgroundColor: '#FFD166' }}>
+                            Log in
+                        </button>
+                    </Link>
+                </div>
             </div>
             <div className={`h-28`}></div>
         </nav>
-    )
+    );
 }
