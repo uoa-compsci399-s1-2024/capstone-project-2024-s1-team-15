ce0d180812990c589cc7d1a48856c378a1a625b8
Hunter Chen <kche617@aucklanduni.ac.nz>
2024-05-02 20:41:17 +1200

    implement auth middleware

--- backend/src/services/auth/auth.service.ts
+++ backend/src/services/auth/auth.service.ts
@@ -1,6 +1,11 @@
-import { JwtPayload, sign, verify } from "jsonwebtoken"
+import { sign, verify } from "jsonwebtoken"
 import { Nullable } from "@/util/types/types"
 import { IUser, UserScope } from "@aapc/types"
+
+export type JWTPayload = {
+    username: string,
+    scopes: UserScope[]
+}
 
 export default class AuthContext {
     private readonly authServiceProvider: IAuthService
@@ -11,26 +16,23 @@ export default class AuthContext {
         this.jwtSecret = jwtSecret
     }
 
-    private issueToken(username: string, scopes: UserScope[]): string {
-        return sign(
-            {
-                username: username,
-                scopes: scopes,
-            },
-            this.jwtSecret
-        )
+    private issueToken(payload: JWTPayload): string {
+        return sign(payload, this.jwtSecret)
     }
 
     async login(user: IUser, password: string): Promise<Nullable<string>> {
         if (await this.authServiceProvider.authenticateUser(user.username, password)) {
-            return this.issueToken(user.username, user.scopes)
+            return this.issueToken({
+                username: user.username,
+                scopes: user.scopes
+            })
         }
         return null
     }
 
-    verifyToken(token: string): Nullable<string | JwtPayload> {
+    verifyToken(token: string): Nullable<JWTPayload> {
         try {
-            return verify(token, this.jwtSecret)
+            return <JWTPayload>verify(token, this.jwtSecret)
         } catch (err) {
             return null
         }
