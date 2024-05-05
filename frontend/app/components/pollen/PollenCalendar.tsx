664df832dca0fbd861d1c61634b49359baace9c6
Hunter Chen <kche617@aucklanduni.ac.nz>
2024-05-05 17:28:24 +1200

    fix import issue

--- frontend/app/components/pollen/PollenCalendar.tsx
+++ frontend/app/components/pollen/PollenCalendar.tsx
@@ -1,6 +1,6 @@
 import { memo, useEffect, useState } from "react"
 import dayjs from "dayjs"
-import { PollenData } from "@/app/(cms)/pollen/edit/type/PollenDataType"
+import { PollenData } from "@aapc/types"
 import { formatPollenData, FormattedPollenData } from "@/app/(cms)/pollen/components/util/formatData"
 import PollenTypeInput from "@/app/components/pollen/PollenTypeInput"
 import DateInput from "@/app/components/pollen/DateInput"
