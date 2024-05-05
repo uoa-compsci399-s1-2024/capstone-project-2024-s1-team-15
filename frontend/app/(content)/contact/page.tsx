1d43ff16d3ce28e264e9f5b01648e66fedf3598f
stanleywu <stanleywu90@gmail.com>
2024-05-05 17:46:17 +1200

    Added styling for Contact us title and form text areas

--- frontend/app/(content)/contact/page.tsx
+++ frontend/app/(content)/contact/page.tsx
@@ -5,7 +5,8 @@ export default function Contact() {
     return (
         <div>
             <div>
-                <h1 className="text-3xl font-bold">Contact Us</h1>
+                <h1 className="text-3xl text-white font-bold bg-purple-300 p-4 inline-block rounded-full w-auto md:text-left">Contact
+                    Us</h1>
             </div>
             <p>Please fill in the form to contact the AAPC team</p>
 
--- frontend/app/components/ContactForm.tsx
+++ frontend/app/components/ContactForm.tsx
@@ -9,43 +9,58 @@ export default function ContactForm() {
                 // onSubmit={handleSubmit}
                 className="py-4 my-4 border-t flex flex-col gap-5"
             >
-                <div className="flex flex-col gap-2">
+                <div className="flex flex-col gap-2 ">
                     <label htmlFor="fullname" className="font-bold text-gray-800">Full Name</label>
-                    <input
-                        type="text"
-                        id="fullname"
-                        minLength={1}
-                        maxLength={50}
-                        required
-                        placeholder="Type your full name here..."
-                        className="shadow-md px-1 py-2 border border-slate-300"
+                    <div
+                        className="flex items-center justify-center group rounded-lg text-sm font-semibold w-full focus-within:bg-gradient-to-r focus-within:from-yellow-300 focus-within:to-purple-500 bg-yellow-300 p-[2px] focus-within:p-[2px] focus-within:shadow-md">
+                        <div className="bg-white w-full py-2 px-3 rounded-lg flex justify-between items-center group">
+                            <input
+                                type="text"
+                                id="fullname"
+                                minLength={1}
+                                maxLength={50}
+                                placeholder="Type your full name here..."
+                                className="w-full h-6 group outline-none placeholder-gray-500 "
 
-                    />
+                            />
+                        </div>
+                    </div>
                 </div>
 
                 <div className="flex flex-col gap-2">
                     <label htmlFor="email" className="font-bold text-gray-800">Email</label>
-                    <input
-                        type="text"
-                        id="email"
-                        minLength={6}
-                        maxLength={50}
-                        required
-                        placeholder="Type your email here..."
-                        className="shadow-md px-1 py-2 border border-slate-300"
-                    />
+                    <div
+                        className="flex items-center justify-center group rounded-lg text-sm font-semibold w-full focus-within:bg-gradient-to-r focus-within:from-yellow-300 focus-within:to-purple-500 bg-yellow-300 p-[2px] focus-within:p-[2px] focus-within:shadow-md">
+                        <div className="bg-white w-full py-2 px-3 rounded-lg flex justify-between items-center group">
+                            <input
+                                type="text"
+                                id="email"
+                                minLength={6}
+                                maxLength={50}
+                                required
+                                placeholder="Type your email here..."
+                                className="w-full h-6 group outline-none placeholder-gray-500 "
+                            />
+                        </div>
+                    </div>
+
                 </div>
 
                 <div className="flex flex-col gap-2">
                     <label htmlFor="message" className="font-bold text-gray-800">Your Message</label>
-                    <textarea
-                        id="message"
-                        minLength={2}
-                        maxLength={500}
-                        required
-                        placeholder="Type your message here..."
-                        className="shadow-md px-1 py-2 border border-slate-300 h-32"
-                    ></textarea>
+                    <div
+                        className="flex items-center justify-center group rounded-lg text-sm font-semibold w-full focus-within:bg-gradient-to-r focus-within:from-yellow-300 focus-within:to-purple-500 bg-yellow-300 p-[2px] focus-within:p-[2px] focus-within:shadow-md">
+                        <div className="bg-white w-full py-2 px-3 rounded-lg flex justify-between items-center group">
+                        <textarea
+                            id="message"
+                            minLength={2}
+                            maxLength={500}
+                            required
+                            placeholder="Type your message here..."
+                            className="w-full group outline-none placeholder-gray-500 h-32"
+                        ></textarea>
+                        </div>
+                    </div>
                 </div>
 
                 <button className={"button w-48"} type="submit">
