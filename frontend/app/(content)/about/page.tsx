2eb3b327f96be520169b3de4ce4a16a753e33c06
junjunjun1 <lpar900@aucklanduni.ac.nz>
2024-05-07 00:59:39 +1200

    Update page.tsx

--- frontend/app/(content)/about/page.tsx
+++ frontend/app/(content)/about/page.tsx
@@ -7,9 +7,15 @@ export default function About() {
         <div>
             <h1 className="text-3xl text-white font-bold bg-purple-300 p-4 inline-block
             rounded-full w-auto md:text-left">About Us</h1>
-            <p className="mt-3">
-                Who we are and what we do.
-            </p>
+            <p>AAPC stands for the Aotearoa Airborne Pollen Collective. It includes a group of researchers working together to understand more about pollen and its impacts on health in New Zealand.
+
+It was founded in 2024 and includes researchers who have spent years working in this area of science and health. Researchers are from different parts of New Zealand.</p>
+            
+<h2>Meet the Researchers!</h2>
+
+<h2>Academic Organisations</h2>
+
+<p>These organisations are backing the AAPC and providing the tools, technologies and support to continue research into various pollen-related topics.</p>
             <AboutPage />
         </div>
     )
