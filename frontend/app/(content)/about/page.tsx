1c280bac8179dfa513ba2fea4a508f45e6a5e59d
stanleywu <stanleywu90@gmail.com>
2024-05-07 00:05:04 +1200

    Created and added some styles to the researchers card and made the footer for organisations that partner with AAPC

--- frontend/app/(content)/about/page.tsx
+++ frontend/app/(content)/about/page.tsx
@@ -1,9 +1,16 @@
 import React from "react"
+import ContactForm from "@/app/components/ContactForm"
+import AboutPage from "@/app/components/AboutPage"
 
-export default function CreateResearchPage() {
+export default function About() {
     return (
         <div>
-            <h1>About Page</h1>
+            <h1 className="text-3xl text-white font-bold bg-purple-300 p-4 inline-block
+            rounded-full w-auto md:text-left">About Us</h1>
+            <p className="mt-3">
+                Who we are and what we do.
+            </p>
+            <AboutPage />
         </div>
     )
 }
