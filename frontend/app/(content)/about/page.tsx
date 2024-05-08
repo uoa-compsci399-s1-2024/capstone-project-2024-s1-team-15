a6332b708438a06ab72f8702a7ad39404aef9308
stanleywu <stanleywu90@gmail.com>
2024-05-09 00:30:41 +1200

    Formatted news, research, about and contact pages to make the titles consistent across all pages using PageTemplate

--- frontend/app/(content)/about/page.tsx
+++ frontend/app/(content)/about/page.tsx
@@ -1,30 +1,23 @@
 import React from "react"
-import ContactForm from "@/app/components/ContactForm"
 import AboutPage from "@/app/components/AboutPage"
+import PageTemplate from "@/app/components/PageContentTemplate"
 
 export default function About() {
     return (
-        <div>
-            <h1
-                className="page-title">
-                About Us
-            </h1>
-            <p>
+        <PageTemplate>
+            <PageTemplate.PageName><div className="page-title">About Us</div></PageTemplate.PageName>
+
+            <PageTemplate.PageExplanation>
                 AAPC stands for the Aotearoa Airborne Pollen Collective. It includes a group of researchers working
                 together to understand more about pollen and its impacts on health in New Zealand.<br/>It was founded in
                 2024 and includes researchers who have spent years working in this area of science and health.
                 Researchers are from different parts of New Zealand.
-            </p>
+            </PageTemplate.PageExplanation>
 
             <h2>Meet the Researchers!</h2>
-
-            <h2>Academic Organisations</h2>
-
-            <p>
-                These organisations are backing the AAPC and providing the tools, technologies and support to continue
-                research into various pollen-related topics.
-            </p>
-            <AboutPage />
-        </div>
+            <PageTemplate.RemainingPageContent>
+                <AboutPage />
+            </PageTemplate.RemainingPageContent>
+        </PageTemplate>
     )
 }
