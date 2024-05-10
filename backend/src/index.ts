ea4a868e218ef71e4ac791c3a34b8a244b05d3d6
Arnav <arnard76@gmail.com>
2024-05-10 16:18:01 +1200

    pollen data is quite large so extending limit from 100kb (default) to 5mb
    https://expressjs.com/en/api.html

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,7 +13,7 @@ const app: Express = express()
 const port = process.env.PORT || 3000
 
 // Use JSON parser middleware
-app.use(express.json())
+app.use(express.json({ limit: "5mb" }))
 
 // Use CORS middleware
 app.use(require("cors")())
