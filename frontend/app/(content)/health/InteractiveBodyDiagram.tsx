e8499d8fd56589c47d2373eaec76f0cd71fc5c3b
Arnav <arnard76@gmail.com>
2024-05-16 11:32:45 +1200

    bug: when hovering over interactive body diagram, the zoom results in overflow on x and y
    so user can see scroll bar change when they hover
    
    if they are using a phone, the x-scroll bar appears allowing them to scroll in x direction 😬😬
    
    fix: hide overflow 😎😎

--- frontend/app/(content)/health/InteractiveBodyDiagram.tsx
+++ frontend/app/(content)/health/InteractiveBodyDiagram.tsx
@@ -22,7 +22,7 @@ export default function InteractiveBodyDiagram() {
 
     return (
         <div>
-            <div className={`h-96 w-fit opacity-gradient relative group`}>
+            <div className="h-96 w-fit overflow-hidden opacity-gradient relative group">
                 <p className={`absolute group-hover:opacity-0 ${selectedBodyPart ? "opacity-0" : ""} transition-all`}>
                     Hover on me!
                 </p>
