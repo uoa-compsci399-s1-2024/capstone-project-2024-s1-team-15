df5d8fb7fffba0e03b7ac69ff531d602c4412766
stanleywu <stanleywu90@gmail.com>
2024-05-16 15:13:26 +1200

    Applied styles to footer directly

--- frontend/app/components/Copyright.tsx
+++ frontend/app/components/Copyright.tsx
@@ -2,10 +2,8 @@ export default function Copyright() {
 
     return (
 
-        <footer>
-            <div className="w-full mx-auto p-1 md:py-8">
+        <footer className="w-full mx-auto p-1 md:py-8">
                 <span className="block text-sm text-gray-500 text-center dark:text-gray-400">© 2024 AAPC - Aotearoa Airborne Pollen Collective</span>
-            </div>
         </footer>
 
     );
