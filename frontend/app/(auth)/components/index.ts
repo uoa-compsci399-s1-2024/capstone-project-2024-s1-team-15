5488e8df175e53ed89d53e10fdbf29ca16c8e355
Arnav <arnard76@gmail.com>
2024-05-18 12:13:43 +1200

    create a change password form
    which requests the backend endpoint `PUT /auth/password/:username`

--- frontend/app/(auth)/components/index.ts
+++ frontend/app/(auth)/components/index.ts
@@ -2,6 +2,7 @@ import AuthDashboard from "@/app/(auth)/components/AuthDashboard"
 import AuthLayout from "@/app/(auth)/components/AuthLayout"
 import AuthProvider from "@/app/(auth)/components/AuthProvider"
 import LoginForm from "@/app/(auth)/components/LoginForm"
+import ChangePasswordForm from "@/app/(auth)/components/ChangePasswordForm"
 import UserScopeLabel from "@/app/(auth)/components/UserScopeLabel"
 
-export { AuthDashboard, AuthLayout, AuthProvider, LoginForm, UserScopeLabel }
+export { AuthDashboard, AuthLayout, AuthProvider, LoginForm, ChangePasswordForm, UserScopeLabel }
