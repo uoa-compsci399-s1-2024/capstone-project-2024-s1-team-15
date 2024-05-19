b34cb06a17dd3d221b5db56e2867b1a63cfb2bd2
junjunjun1 <lpar900@aucklanduni.ac.nz>
2024-05-19 21:22:17 +1200

    Update page.tsx

--- frontend/app/(content)/about/page.tsx
+++ frontend/app/(content)/about/page.tsx
@@ -8,7 +8,9 @@ export default function About() {
 
             <p>
             Aotearoa Airborne Pollen Collaborative (AAPC) is a research collaboration between researchers at
-            the University of Auckland Medical School, Victoria University of Wellington, and Massey University.
+            the University of Auckland Medical School, Victoria University of Wellington, and Massey University. AAPC has its beginnings in 2022, when overlapping Research interests brought Associate Professor
+            Amy Chan, Associate Professor Stuti Misra, Professor Rewi Newnham and Dr Kat Holt together in a
+            Zoom room.
             </p>
 
             <h2 className={"text-center"}>Meet the Researchers!</h2>
