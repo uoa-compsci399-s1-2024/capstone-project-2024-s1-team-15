5630e7d2c99599db462785468588c0c2e928812e
Gautham-27 <ggun897@aucklanduni.ac.nz>
2024-05-20 03:13:04 +1200

    Update research.ts
    Added deleteResearch function and revalidate function and tags to fetch updated data for the frontend

--- frontend/app/services/research.ts
+++ frontend/app/services/research.ts
@@ -3,13 +3,13 @@ import { API_URI } from "@/app/lib/consts"
 import { ArticleOut, Nullable, Result } from "@/app/lib/types"
 import { FetchOptions, getHeaders } from "@/app/services/lib/util"
 import { fail, success } from "@/app/lib/util";
-import revalidateArticle from "./revalidator";
+import {revalidateResearch} from "./lib/revalidator";
 
 export async function getResearchById(id: string, options?: FetchOptions): Promise<Nullable<IArticle>> {
     const response = await fetch(API_URI + `/content/research/${id}`, {
         method: "get",
         headers: getHeaders(options),
-        next: { tags: ["article"]}
+        next: { tags: ["research"]}
     })
     if (response.status === 404) {
         return null
@@ -20,7 +20,8 @@ export async function getResearchById(id: string, options?: FetchOptions): Promi
 export async function getAllResearch(options?: FetchOptions): Promise<IPaginator<IArticle>> {
     const response = await fetch(API_URI + `/content/research/`, {
         method: "get",
-        headers: getHeaders(options)
+        headers: getHeaders(options),
+        next: { tags: ["research"]}
     })
     return new Paginator(Article, await response.json())
 }
@@ -34,6 +35,7 @@ export async function publishResearch(a: ArticleOut, options?: FetchOptions): Pr
     if (response.status >= 400) {
         return fail((await response.json()).message)
     }
+    revalidateResearch()
     return success(new Article(await response.json()))
 }
 
@@ -46,6 +48,15 @@ export async function editResearch(id: string, a: ArticleOut, options?: FetchOpt
     if (response.status >= 400) {
         return fail((await response.json()).message)
     }
-    revalidateArticle()
+    revalidateResearch()
     return success(new Article(await response.json()))
+}
+
+export async function deleteResearch(id: string, options?: FetchOptions): Promise<Response> {
+    const response = await fetch(API_URI + `/content/research/${id}`, {
+        method: "delete",
+        headers: getHeaders(options),
+    })
+    revalidateResearch()
+    return response
 }
