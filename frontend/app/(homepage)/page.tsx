37274501672e37b0a49eca3d8972a0c0595ff4c8
Hunter Chen <kche617@aucklanduni.ac.nz>
2024-05-23 05:53:10 +1200

    remove button compendium from homepage

--- frontend/app/(homepage)/page.tsx
+++ frontend/app/(homepage)/page.tsx
@@ -3,8 +3,6 @@ import Image from "next/image"
 import yellowback from "../public/homepageback.svg"
 import pollenimg from "../public/pollenimg.svg"
 import MessageFromQuery from "@/app/components/MessageFromQuery"
-import Button from "@/app/components/Button"
-import icons from "@/app/lib/icons"
 
 export default function Home() {
     return (
@@ -21,37 +19,6 @@ export default function Home() {
                 <h1 className="text-3xl">Welcome to</h1>
                 <h1 className="text-8xl text-center">Aotearoa Airborne<br />Pollen Collective</h1>
                 <p className="text-2xl ml-30 text-center">Clearing the Air: Your Source for Pollen Awareness</p>  
-            </div>
-            <div className={"space-y-4 mt-8"}>
-                <div className={"flex flex-row gap-x-4"}>
-                    <Button text={"Sign up"} icon={icons.signup}/>
-                    <Button text={"Log in"} theme={"secondary"} icon={icons.login}/>
-                    <Button text={"My Account"} icon={icons.user}/>
-                    <Button text={"Log out"} theme={"secondary"} icon={icons.logout}/>
-                </div>
-                <div className={"flex flex-row gap-x-4"}>
-                    <Button text={"Add"} theme={"green"} icon={icons.add}/>
-                    <Button text={"Delete"} theme={"red"} icon={icons.trash}/>
-                    <Button text={"Previous"} theme={"secondary"} icon={icons.back} leftIcon/>
-                    <Button text={"Next"} theme={"secondary"} icon={icons.forward}/>
-                </div>
-                <div className={"flex flex-row gap-x-4"}>
-                    <Button text={"Very Wide Button"} theme={"secondary"} className={"min-w-96"}/>
-                    <Button text={"small"} theme={"secondary"}/>
-                    <Button theme={"green"} icon={icons.information}/>
-                    <Button theme={"red"} icon={icons.close}/>
-                </div>
-                <div className={"flex flex-row gap-x-4"}>
-                    <Button theme={"cms"} text={"Add Article"} icon={icons.add}/>
-                    <Button theme={"cms"} text={"Edit Article"} icon={icons.edit}/>
-                    <Button theme={"cms"} text={"Delete Article"} icon={icons.trash}/>
-                </div>
-                <div className={"flex flex-row gap-x-4"}>
-                    <Button disabled text={"Disabled Button"}/>
-                    <Button theme={"secondary"} disabled text={"Disabled Button"}/>
-                    <Button theme={"red"} disabled text={"Disabled Button"}/>
-                    <Button theme={"green"} disabled text={"Disabled Button"}/>
-                </div>
             </div>
         </div>
     )
