023c855dc0882fe7a7578071a7dea25678560ec1
Hunter <kche617@aucklanduni.ac.nz>
2024-05-23 15:53:50 +1200

    fix merge conflicts

--- frontend/app/components/slider/Slider.tsx
+++ frontend/app/components/slider/Slider.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react"
-import NextSlideArrow from "./NextSlideArrow.svg"
-import Image from "next/image"
+import Button from "@/app/components/Button";
+import icons from "@/app/lib/icons";
 
 type Props = {
     slides: React.JSX.Element[]
@@ -15,24 +15,23 @@ function Slider({ slides, onSelectedSlideIndexChange }: Props) {
     }, [onSelectedSlideIndexChange, selectedSlideIndex])
 
     return (
-        <div className="flex gap-4 ">
-            <button
-                className={`w-10 hover:bg-slate-200 transition-colors rounded-md ${selectedSlideIndex === 0 ? "invisible" : ""}`}
-                onClick={() => setSelectedSlideIndex((curr) => curr - 1)}>
-                <Image
-                    src={NextSlideArrow}
-                    alt="Previous slide arrow icon"
-                    className="w-full h-full origin-center rotate-180"
-                />
-            </button>
+        <div className="flex gap-4">
+            <Button
+                theme={"secondary"}
+                disabled={selectedSlideIndex === 0}
+                onClick={() => setSelectedSlideIndex(c => c - 1)}
+                icon={icons.back}
+                leftIcon
+            />
 
-            <div className="flex-1 text-balance">{slides[selectedSlideIndex]}</div>
+            {slides[selectedSlideIndex]}
 
-            <button
-                className={`w-10 hover:bg-slate-200 transition-colors rounded-md ${selectedSlideIndex === slides.length - 1 ? "invisible" : ""}`}
-                onClick={() => setSelectedSlideIndex((curr) => curr + 1)}>
-                <Image src={NextSlideArrow} alt="Next slide arrow icon" className="w-full h-full" />
-            </button>
+            <Button
+                theme={"secondary"}
+                disabled={selectedSlideIndex === (slides.length - 1)}
+                onClick={() => setSelectedSlideIndex(c => c + 1)}
+                icon={icons.forward}
+            />
         </div>
     )
 }
