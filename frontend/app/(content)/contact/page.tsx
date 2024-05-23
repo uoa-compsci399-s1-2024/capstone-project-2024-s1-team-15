126a8fecce3c0957ce24ffa9b135652e1c2a7710
saamuelkim <samuel.k.0227@gmail.com>
2024-05-23 16:24:19 +1200

    background graphics on contacts page is responsive

--- frontend/app/(content)/contact/page.tsx
+++ frontend/app/(content)/contact/page.tsx
@@ -1,21 +1,24 @@
-import React from "react"
+import React from "react";
 import Image from "next/image";
-import ContactForm from "@/app/components/ContactForm"
-import pollenImg from "@/app/public/pollenimg.svg"
+import ContactForm from "@/app/components/ContactForm";
+import pollenImg from "@/app/public/pollenimg.svg";
 
 export default function Contact() {
     return (
         <>
-            <h1 className="page-title">
+            <h1 className="page-title relative z-20">
                 Contact us
             </h1>
-            <div className="p-8 bg-accent-light rounded-2xl shadow-xl max-w-[720px]">
-                <ContactForm/>
+            <div className="p-8 bg-accent-light rounded-2xl shadow-xl max-w-[720px] relative z-20">
+                <ContactForm />
             </div>
-            <div className="fixed inset-0 z-[-1] pl-96 ml-60">
-                <Image src={pollenImg} alt="pollenimg"
-                    className="-mt-20 ml-20" />
+            <div className="fixed inset-0 z-10">
+                <div className="relative w-full h-full overflow-hidden">
+                    <div className="absolute top-[-80px] right-0 md:w-auto md:h-auto  duration-500 hover:translate-y-[-10px] hover:translate-x-[-10px]">
+                        <Image src={pollenImg} alt="pollenimg" className="object-cover" />
+                    </div>
+                </div>
             </div>
         </>
-    )
+    );
 }
