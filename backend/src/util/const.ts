926bb55ade318833e2465d9a71a214a3b249436a
Hunter Chen <kche617@aucklanduni.ac.nz>
2024-05-26 18:23:47 +1200

    set article publisher from auth request headers

--- backend/src/util/const.ts
+++ backend/src/util/const.ts
@@ -1,4 +1,4 @@
-import { User, UserScope } from "@aapc/types"
+import { UserScope } from "@aapc/types"
 
 export const DEFAULT_PER_PAGE = 15
 export const DEFAULT_MAX_PER_PAGE = 250
@@ -6,14 +6,6 @@ export const DEFAULT_ID_LENGTH = 8
 export const DEFAULT_IMAGE_ID_LENGTH = 12
 export const STATIC_FILE_DIRECTORY = "static"
 export const IMAGE_UPLOAD_LIMIT = 15 * 1024 * 1024  // 15 MB
-
-export const DUMMY_USER = new User({
-    username: "foobar",
-    displayName: "John Doe",
-    email: "john.doe@example.com",
-    verified: true,
-    registeredAt: new Date().toISOString(),
-})
 
 export const SCOPES = {
     user: [UserScope.user, UserScope.regular, UserScope.premium, UserScope.maintainer, UserScope.admin],
