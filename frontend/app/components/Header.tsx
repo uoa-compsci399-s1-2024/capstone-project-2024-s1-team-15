4447e1695ba47731050e05fbe78e137b77dd31e1
Arnav <arnard76@gmail.com>
2024-05-28 01:00:42 +1200

    formatting files

--- frontend/app/components/Header.tsx
+++ frontend/app/components/Header.tsx
@@ -49,10 +49,10 @@ export default function Header(): React.JSX.Element {
     return (
         <>
             <div className={"lg:hidden"}>
-                <MobileNavModal onClose={() => setNavIcon(defaultNavIcon)} ref={mobileNavModalRef}/>
+                <MobileNavModal onClose={() => setNavIcon(defaultNavIcon)} ref={mobileNavModalRef} />
             </div>
-            <div className={'md:hidden'}>
-                <MobileAuthModal onClose={() => setAuthIcon(defaultAuthIcon)} ref={mobileAuthModalRef}/>
+            <div className={"md:hidden"}>
+                <MobileAuthModal onClose={() => setAuthIcon(defaultAuthIcon)} ref={mobileAuthModalRef} />
             </div>
             <div
                 className={`fixed top-0 w-full flex items-center z-30 bg-gradient-to-b from-background to-background-light
@@ -62,45 +62,47 @@ export default function Header(): React.JSX.Element {
                 lg:pl-12 lg:pr-16
             `}>
                 {/* Nav Dropdown - Only shown on mobile, tablet & desktop narrow viewports */}
-                <button className={`block lg:hidden
+                <button
+                    className={`block lg:hidden
                     h-header-mobile w-header-mobile
                     sm:h-header-tablet sm:w-header-tablet
                     md:w-header-desktop md:h-header-desktop
-                `} onClick={() => {
-                    toggleMobileNavModal()
-                    hideMobileAuthModal()
-                }}>
-                    <div className={"m-1.5 p-2.5 sm:m-2 sm:p-3 md:m-2.5 md:p-4 rounded-xl"}>
-                        {navIcon}
-                    </div>
+                `}
+                    onClick={() => {
+                        toggleMobileNavModal()
+                        hideMobileAuthModal()
+                    }}>
+                    <div className={"m-1.5 p-2.5 sm:m-2 sm:p-3 md:m-2.5 md:p-4 rounded-xl"}>{navIcon}</div>
                 </button>
 
                 {/* Main Logo */}
                 <div className={"md:flex-grow"}>
                     <Link href={"/"} className="text-black p-4">
-                    <span
-                        className={"font-bold text-2xl sm:text-4xl md:text-5xl text-black tracking-tight select-none"}>
-                        AAPC
-                    </span>
+                        <span
+                            className={
+                                "font-bold text-2xl sm:text-4xl md:text-5xl text-black tracking-tight select-none"
+                            }>
+                            AAPC
+                        </span>
                     </Link>
                 </div>
 
                 {/* Auth Dashboard - Only shown on desktop viewports */}
                 <div className={"hidden md:block justify-self-end"}>
-                    <AuthDashboard dashboardLocation={"desktop-nav"}/>
+                    <AuthDashboard dashboardLocation={"desktop-nav"} />
                 </div>
 
                 {/* Auth Dropdown - Only shown on mobile & tablet viewports */}
-                <button className={`block md:hidden
+                <button
+                    className={`block md:hidden
                     h-header-mobile w-header-mobile
                     sm:h-header-tablet sm:w-header-tablet
-                `} onClick={() => {
-                    toggleMobileAuthModal()
-                    hideMobileNavModal()
-                }}>
-                    <div className={"m-1.5 p-2.5 sm:m-2 sm:p-3 md:m-2.5 md:p-4 rounded-xl"}>
-                        {authIcon}
-                    </div>
+                `}
+                    onClick={() => {
+                        toggleMobileAuthModal()
+                        hideMobileNavModal()
+                    }}>
+                    <div className={"m-1.5 p-2.5 sm:m-2 sm:p-3 md:m-2.5 md:p-4 rounded-xl"}>{authIcon}</div>
                 </button>
             </div>
         </>
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -5,37 +5,37 @@ const config: Config = {
         "./pages/**/*.{js,ts,jsx,tsx,mdx}",
         "./components/**/*.{js,ts,jsx,tsx,mdx}",
         "./app/**/*.{js,ts,jsx,tsx,mdx}",
-        "./node_modules/flowbite-react/lib/**/*.js"
+        "./node_modules/flowbite-react/lib/**/*.js",
     ],
     theme: {
         screens: {
             // Below 540: Display content in mobile style
             // Expand content div
-            'sm': "540px", // Above 540: Display content in tablet style
+            sm: "540px", // Above 540: Display content in tablet style
             // Expand content div
-            'md': "832px", // Above 832: Display content in desktop style
+            md: "832px", // Above 832: Display content in desktop style
             // Expand content div
-            'lg': "1120px", // Above 1120 (content-min + nav): Display flower nav
+            lg: "1120px", // Above 1120 (content-min + nav): Display flower nav
             // Expand content div
-            'xl': "1440px", // Above 1440 (content-max + nav):
+            xl: "1440px", // Above 1440 (content-max + nav):
             // Expand left margin
-            '2xl': "1728px" // Above 1728 (content-max + nav + nav)
+            "2xl": "1728px", // Above 1728 (content-max + nav + nav)
             // Expand both margins
         },
         extend: {
             spacing: {
-                "pc": "12px", // pc -> padding-content
+                pc: "12px", // pc -> padding-content
                 "pc-sm": "16px",
                 "pc-md": "24px",
 
                 "content-max": "1152px",
                 "content-min": "832px",
-                "nav": "288px",
+                nav: "288px",
 
                 "header-desktop": "96px",
                 "header-tablet": "72px",
                 "header-mobile": "56px",
-                "footer": "56px"
+                footer: "56px",
             },
             fontFamily: {
                 sans: ["var(--font-inter)"],
@@ -53,31 +53,31 @@ const config: Config = {
                         img: {
                             borderRadius: "0.5rem",
                             marginTop: "0",
-                            marginBottom: "0"
+                            marginBottom: "0",
                         },
                         a: {
                             color: "rgb(53,100,239)",
                             "&:hover": {
-                                color: "rgb(26,78,217)"
-                            }
-                        }
+                                color: "rgb(26,78,217)",
+                            },
+                        },
                     },
                 },
             },
             colors: {
-                "primary": "#FFD166",
+                primary: "#FFD166",
                 "primary-light": "#fff0ce",
-                "secondary": "#9CA3AF",
-                "tertiary": "#FFE9B6",
+                secondary: "#9CA3AF",
+                tertiary: "#FFE9B6",
                 "accent-light": "#F5EDFF",
                 "accent-dark": "#E6CFFF",
 
-                "admin": "#f35d5d",
-                "maintainer": "#37a0ec",
-                "premium": "#ffd78f",
-                "user": "#c9c9c9",
+                admin: "#f35d5d",
+                maintainer: "#37a0ec",
+                premium: "#ffd78f",
+                user: "#c9c9c9",
 
-                "background": "#fafafa",
+                background: "#fafafa",
                 "background-light": "rgba(250,250,250,0.8)",
             },
             borderWidth: {
@@ -91,9 +91,7 @@ const config: Config = {
             },
         },
     },
-    plugins: [
-        require("@tailwindcss/typography")
-    ],
+    plugins: [require("@tailwindcss/typography")],
     darkMode: "selector",
 }
 export default config
