fd743d57ebce6f6adbecd6e484a950f59b53c833
Hunter Chen <kche617@aucklanduni.ac.nz>
2024-05-28 17:30:02 +1200

    resolve merge conflicts

--- frontend/app/(content)/(articles)/research/page.tsx
+++ frontend/app/(content)/(articles)/research/page.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { getMetadata } from "@/app/lib/util"
 import Paginator from "@/app/components/Paginator"
-import DisplayAllArticles from "../components/DisplayAllArticles"
+import DisplayAllArticles from "@/app/(content)/(articles)/components/DisplayAllArticles"
 
 export const metadata = getMetadata("All Research")
 
@@ -11,7 +11,7 @@ export default function AllResearchPage() {
             <div className="max-w-screen-xl mr-auto items-center justify-between gap-x-4 sm:flex">
                 <h1 className="page-title">All Research</h1>
             </div>
-            <DisplayAllArticles articleType="research" />
+            <DisplayAllArticles articleType="research"/>
             <Paginator />
         </>
     )
