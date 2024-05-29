d0c80c1267fc2dc1d6ab64e04fb2266f9cdea85b
Arnav <arnard76@gmail.com>
2024-05-29 19:47:14 +1200

    fix bug created on contact page

--- frontend/app/(content)/contact/page.tsx
+++ frontend/app/(content)/contact/page.tsx
@@ -6,13 +6,13 @@ import pollenImg from "@/app/public/pollenimg.svg"
 export default function Contact() {
     return (
         <>
-            <h1 className="page-title relative z-20">
+            <h1 className="page-title relative z-10">
                 Contact us
             </h1>
-            <div className="p-8 bg-accent-light rounded-2xl shadow-xl max-w-[720px] relative z-20">
+            <div className="p-8 bg-accent-light rounded-2xl shadow-xl max-w-[720px] relative z-10">
                 <ContactForm />
             </div>
-            <div className="fixed inset-0 z-10">
+            <div className="fixed inset-0">
                 <div className="relative w-full h-full overflow-hidden">
                     <div className="absolute top-[-80px] right-0 md:w-auto md:h-auto  duration-500 hover:translate-y-[-10px] hover:translate-x-[-10px]">
                         <Image src={pollenImg} alt="pollenimg" className="object-cover" />
