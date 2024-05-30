7a38a76d00a38c09e1edd799188ed2cc7e0c5a29
saamuelkim <samuel.k.0227@gmail.com>
2024-05-31 03:52:53 +1200

    new homepage design

--- frontend/app/(homepage)/page.tsx
+++ frontend/app/(homepage)/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import yellowback from "../public/homepageback.svg";
+import yellowblob from "../public/yellowblob.svg";
 import pollenimg from "../public/pollenimg.svg";
 import MessageFromQuery from "@/app/components/MessageFromQuery";
 
@@ -14,6 +15,11 @@ export default function Home() {
                 <div className="fixed">
                     <Image src={yellowback} alt="backgroundimg" className="pl-40 pt-5" />
                 </div>
+                <div className="fixed" style={{ width: '800px', height: '400px' }}>
+    <Image src={yellowblob} alt="yellowblob" className="ml-[-650px] mt-[200px]" />
+</div>
+
+
                 <div className="fixed inset-0 pl-96 ml-60">
                     <div className="relative w-full h-full overflow-hidden">
                         <div className="-mt-20 ml-20 absolute top-[-80px] right-0 md:w-auto md:h-auto duration-500 hover:translate-y-[-10px] hover:translate-x-[-10px]">
