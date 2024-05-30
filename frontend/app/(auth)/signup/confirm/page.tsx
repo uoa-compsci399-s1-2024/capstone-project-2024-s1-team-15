ab0915135f6797bcea3a1cac2caecd8765206d55
Hunter Chen <kche617@aucklanduni.ac.nz>
2024-05-30 22:10:51 +1200

    extract ConfirmationCodeInput

--- frontend/app/(auth)/signup/confirm/page.tsx
+++ frontend/app/(auth)/signup/confirm/page.tsx
@@ -1,11 +1,12 @@
 "use client"
 
-import React, { ChangeEvent, useEffect, useState } from "react"
+import React, { useEffect, useState } from "react"
 import { useRouter } from "next/navigation"
 import { Nullable } from "@/app/lib/types"
 import Button from "@/app/components/Button"
 import icons from "@/app/lib/icons"
 import { confirmRegister } from "@/app/services/auth"
+import ConfirmationCodeInput from "@/app/components/ConfirmationCodeInput"
 
 export default function SignupConfirm() {
     const [username, setUsername] = useState<Nullable<string>>(null)
@@ -46,14 +47,6 @@ export default function SignupConfirm() {
         }
     }
 
-    const codeInputHandler = (e: ChangeEvent<HTMLInputElement>) => {
-        if (e.target.value.length === 6 && !isNaN(Number(e.target.value))) {
-            setIsValidCode(true)
-            return
-        }
-        setIsValidCode(false)
-    }
-
     return (
         <div>
             <h1 className={"page-title"}>Sign up</h1>
@@ -63,17 +56,7 @@ export default function SignupConfirm() {
             <form className={"space-y-4"} action={submitConfirmSignup}>
                 <div>
                     <label className={"form-label"} htmlFor={"code"}>Confirmation Code</label>
-                    <input
-                        id={"code"}
-                        name={"code"}
-                        className={`form-input block
-                            !text-3xl !h-16 max-w-56 text-gray-700 tracking-[7px] font-medium !rounded-full text-center
-                        `}
-                        type={"text"}
-                        placeholder={"000000"}
-                        maxLength={6}
-                        onChange={codeInputHandler}
-                    />
+                    <ConfirmationCodeInput setIsValidCode={setIsValidCode}/>
                 </div>
                 <Button
                     disabled={!isValidCode || pending}
