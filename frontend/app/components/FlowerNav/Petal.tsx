bf84b54a7aa06cba9006fbbe50f9517c2c765361
Hunter Chen <kche617@aucklanduni.ac.nz>
2024-06-01 20:31:48 +1200

    move PetalImage.svg

--- frontend/app/components/FlowerNav/Petal.tsx
+++ frontend/app/components/FlowerNav/Petal.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image"
 import Link from "next/link"
-import PetalImageFile from "./PetalImage.svg"
+import PetalImageFile from "../../public/PetalImage.svg"
 
 import { Page } from "@/app/lib/types"
 
