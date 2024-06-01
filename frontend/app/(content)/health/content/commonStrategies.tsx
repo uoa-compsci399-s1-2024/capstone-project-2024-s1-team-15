f6959b0f1665f8a171557c57acd8946899535a3a
Hunter Chen <kche617@aucklanduni.ac.nz>
2024-06-01 20:29:04 +1200

    optimize redeye picture

--- frontend/app/(content)/health/content/commonStrategies.tsx
+++ frontend/app/(content)/health/content/commonStrategies.tsx
@@ -1,27 +1,42 @@
-import WashingMachineImage from "../../../public/health/washingMachine.svg"
-import DustFurniture from "../../../public/health/dustFurniture.svg"
-import NasalRinseImage from "../../../public/health/rinseNasalPassages.svg"
-import WashHandsImage from "../../../public/health/washHands.svg"
-import ShutWindowsImages from "../../../public/health/shutWindows.svg"
-import AvoidSmokingImage from "../../../public/health/avoidSmoking.svg"
+import WashingMachineImage from "@/app/public/health/washingMachine.svg"
+import DustFurniture from "@/app/public/health/dustFurniture.svg"
+import NasalRinseImage from "@/app/public/health/rinseNasalPassages.svg"
+import WashHandsImage from "@/app/public/health/washHands.svg"
+import ShutWindowsImages from "@/app/public/health/shutWindows.svg"
+import AvoidSmokingImage from "@/app/public/health/avoidSmoking.svg"
+import RedEyeImage from "@/app/public/health/redeye.png"
 import NasalRinseExplanation from "@/app/(content)/health/components/NasalRinse"
-import RedEyeImage from "../../../public/health/redeye.svg"
 import ArtificialTearsExplanation from "@/app/(content)/health/components/ArtificialTears"
 
 const strategies = {
     "Wash bedding regularly": {
         image: WashingMachineImage,
-        explanation: null,
+        explanation: null
     },
-    "Dust frequently": { image: DustFurniture, explanation: null },
+    "Dust frequently": {
+        image: DustFurniture,
+        explanation: null
+    },
     "Rinse nasal passages": {
         image: NasalRinseImage,
-        explanation: <NasalRinseExplanation />,
+        explanation: <NasalRinseExplanation/>
     },
-    "Wash hands after playing with pets": { image: WashHandsImage, explanation: null },
-    "Shut windows during pollen season": { image: ShutWindowsImages, explanation: null },
-    "Avoid smoke and fragrances": { image: AvoidSmokingImage, explanation: null },
-    "Use artificial tears": { image: RedEyeImage, explanation: <ArtificialTearsExplanation /> },
+    "Wash hands after playing with pets": {
+        image: WashHandsImage,
+        explanation: null
+    },
+    "Shut windows during pollen season": {
+        image: ShutWindowsImages,
+        explanation: null
+    },
+    "Avoid smoke and fragrances": {
+        image: AvoidSmokingImage,
+        explanation: null
+    },
+    "Use artificial tears": {
+        image: RedEyeImage,
+        explanation: <ArtificialTearsExplanation/>
+    }
 }
 
 export default strategies
